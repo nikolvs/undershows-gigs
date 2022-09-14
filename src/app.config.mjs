2870eabb38a6174f9e10c110268a2abd19c2a67d
Nikolas Silva <nikolas.rsilva@gmail.com>
2022-09-14 17:05:57 -0300

    Update site configs

--- src/app.config.mjs
+++ src/app.config.mjs
@@ -2,7 +2,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 /**
+ * Site configuration.
+ */
+export const SITE_URL = process.env.SITE_URL;
+export const SITE_TITLE = process.env.SITE_TITLE;
+
+/**
  * Locale configuration.
  */
-export const DEFAULT_LOCALE = process.env.DEFAULT_LOCALE ?? 'pt-BR'
+export const DEFAULT_LOCALE = process.env.DEFAULT_LOCALE ?? 'pt-BR';
 export const SUPPORTED_LANGUAGES = ['pt-BR', 'en'];
