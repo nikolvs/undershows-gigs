f89c30251d0b1597ba6a2c2146e34fe4ac7bd4c0
Nikolas Silva <nikolas.rsilva@gmail.com>
2022-09-14 16:54:11 -0300

    Add tailwind and daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,21 @@ import { DEFAULT_LOCALE, SUPPORTED_LANGUAGES } from './src/app.config';
 
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL,
   integrations: [
     astroI18next(),
+    tailwind(),
     sitemap({
       changefreq: 'daily',
       lastmod: new Date(),
       priority: 0.7,
       i18n: {
         defaultLocale: DEFAULT_LOCALE,
-        locales: SUPPORTED_LANGUAGES.reduce((locales, lang) => ({
-          ...locales,
+        locales: SUPPORTED_LANGUAGES.reduce((locales, lang) => ({ ...locales,
           [lang]: lang.toLowerCase()
         }), {})
       }
