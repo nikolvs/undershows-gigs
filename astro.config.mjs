c216769fac453ff31299d7a70e974032f528ca77
Nikolas Silva <nikolas.rsilva@gmail.com>
2022-09-14 11:59:40 -0300

    Add sitemap and configure import aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,24 @@
 import { defineConfig } from "astro/config";
+
 import astroI18next from "astro-i18next";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+  site: 'https://undershows.com.br',
+  integrations: [
+    astroI18next(),
+    sitemap({
+      changefreq: 'daily',
+      lastmod: new Date(),
+      priority: 0.7,
+      i18n: {
+        defaultLocale: 'pt-br',
+        locales: {
+          en: 'en',
+          'pt-br': 'pt-BR'
+        }
+      }
+    })
+  ]
 });
