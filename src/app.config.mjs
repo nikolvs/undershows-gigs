8762922fe74077210d3577be45c1963dcc911795
Nikolas Silva <nikolas.rsilva@gmail.com>
2022-09-16 04:39:10 -0300

    Add navbar and lang index page

--- src/app.config.mjs
+++ src/app.config.mjs
@@ -4,11 +4,16 @@ dotenv.config();
 /**
  * Site configuration.
  */
-export const SITE_URL = process.env.SITE_URL;
-export const SITE_TITLE = process.env.SITE_TITLE;
+export const SITE_URL = process.env.SITE_URL ?? 'http://localhost:3000';
+export const SITE_TITLE = process.env.SITE_TITLE ?? 'undershows';
 
 /**
  * Locale configuration.
  */
 export const DEFAULT_LOCALE = process.env.DEFAULT_LOCALE ?? 'pt-BR';
 export const SUPPORTED_LANGUAGES = ['pt-BR', 'en'];
+
+/**
+ * App configuration.
+ */
+export const DEFAULT_STATE = process.env.DEFAULT_STATE ?? 'SP';
