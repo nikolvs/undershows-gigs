e4fd19aee1f8f45a5684037b461513ac8cd37678
Nikolas Silva <nikolas.rsilva@gmail.com>
2022-09-29 03:44:09 -0300

    Add menu list to state selector

--- src/components/state/StateSelector.svelte
+++ src/components/state/StateSelector.svelte
@@ -1,14 +1,32 @@
 <script>
+  import * as R from "ramda";
   import { t, changeLanguage } from "@/utils/i18n";
+  import { outclick } from "@/utils/actions";
 
   export let lang;
   export let states;
   export let selectedState;
 
   changeLanguage(lang);
+
+  let open = false;
+  let stateFilter = "";
+
+  $: re = new RegExp(stateFilter, "i");
+  $: regionSlices = R.pipe(
+    R.filter(R.pipe(R.props(["code", "name"]), R.any(R.test(re)))),
+    R.sortBy(R.prop("region")),
+    R.groupWith(R.eqProps("region")),
+    R.sortWith([R.descend(R.length)]),
+    R.map((states) => ({
+      states,
+      name: R.head(states)?.region,
+    })),
+    R.splitEvery(3)
+  )(states);
 </script>
 
-<div class="state-selector">
+<div class="state-selector" use:outclick={() => (open = false)}>
   <div class="form-control">
     <label class="input-group input-group-md">
       <span>
@@ -19,10 +37,12 @@
         placeholder={t("state.selector.placeholder", {
           state: selectedState.name,
         })}
-        class="input input-md w-full text-lg"
+        class="w-full text-lg input input-md"
+        bind:value={stateFilter}
+        on:focus={() => (open = true)}
       />
 
-      <button class="btn">
+      <button class="btn" on:click={() => (open = true)}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -40,10 +60,41 @@
       </button>
     </label>
   </div>
+
+  <div
+    class="menu menu-compact bg-neutral rounded-b-lg p-4"
+    class:absolute={open}
+    class:hidden={!open}
+  >
+    {#each regionSlices as regionSlice}
+      <div class="flex flex-wrap">
+        {#each regionSlice as region}
+          <ul class="flex-1">
+            <li class="menu-title">
+              <span>{region.name}</span>
+            </li>
+            {#each region.states as state}
+              <li>
+                <a href={state.path}>
+                  <img
+                    src={state.icon}
+                    alt={state.name}
+                    width="20"
+                    class="inline"
+                  />
+                  <span>{state.name}</span>
+                </a>
+              </li>
+            {/each}
+          </ul>
+        {/each}
+      </div>
+    {/each}
+  </div>
 </div>
 
 <style lang="postcss">
   .state-selector {
-    @apply w-full;
+    @apply w-full relative;
   }
 </style>
---
+++ src/utils/actions/index.ts
@@ -0,0 +1,1 @@
+export { default as outclick } from './outclick';
